759bc0b866f04c56f677cad04de8722535608a53
Dmitry2489 <iva.klicunov2489@mail.ru>
2020-05-01 19:14:18 +0300

    refactoring the slider module, inheriting from the Slider class and creating the MainSlider module

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-import Slider from "./modules/slider";
+import MainSlider from "./modules/slider/slider-main";
 import VideoPlayer from "./modules/playVideo";
 
 window.addEventListener("DOMContentLoaded", () => {
     "use string";
 
-    const slider = new Slider(".page", ".next");
+    const slider = new MainSlider({btns: '.next', page: '.page'});
     slider.render();
 
     const player = new VideoPlayer(".showup .play", ".overlay");
